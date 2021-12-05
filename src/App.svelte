8b194420bf3b3f0aa8415bc0b858a222d9719e37
SDC <sdcomms4227@gmail.com>
2021-12-05 23:12:56 +0900

    add Binding, Lifecycle

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,30 @@
 <script>
-	import {Router, Link, Route} from "svelte-routing";
-	import Home from "./routes/Home.svelte";
-	import About from "./routes/About.svelte";
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "./routes/Home.svelte";
+  import About from "./routes/About.svelte";
+  import Binding from "./routes/Binding.svelte";
+  import Lifecycle from "./routes/Lifecycle.svelte";
 
-	export let url = "";
+  export let url = "";
 </script>
 
-<Router url="{url}">
-	<nav>
-		<Link to="/">Home</Link>
-		<Link to="about">About</Link>
-	</nav>
-	<hr>
-	<div>
-		<Route path="/"><Home /></Route>
-		<!-- <Route path="about"><About /></Route> -->
-		<Route path="about" component="{About}" />
-	</div>
+<Router {url}>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="about">About</Link>
+    <Link to="binding">Binding</Link>
+    <Link to="lifecycle">Lifecycle</Link>
+  </nav>
+  <hr />
+  <div>
+    <!-- <Route path="/"><Home /></Route> -->
+    <!-- <Route path="about"><About /></Route> -->
+    <Route path="/" component={Home} />
+    <Route path="about" component={About} />
+    <Route path="binding" component={Binding} />
+    <Route path="lifecycle" component={Lifecycle} />
+  </div>
 </Router>
 
 <style>
-
 </style>
